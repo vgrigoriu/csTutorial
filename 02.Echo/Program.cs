0be069eeb07e05e9e9bf300d14369a1ff02d28eb
Victor Grigoriu <vgrigoriu@gmail.com>
2016-07-29 19:26:57 +0300

    Hello World & Echo

---
+++ 02.Echo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var s = string.Empty;
+            var separator = string.Empty;
+            foreach (var arg in args)
+            {
+                s += separator + arg;
+                separator = " ";
+            }
+
+            Console.WriteLine(s);
+        }
+    }
+}
