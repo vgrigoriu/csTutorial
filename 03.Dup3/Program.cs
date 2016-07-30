be7767c5885c61053baa6f73dfeea9b543dd8fb2
Victor Grigoriu <vgrigoriu@gmail.com>
2016-07-30 22:14:56 +0300

    Dup3: using idiomatic linq

---
+++ 03.Dup3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Dup3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // usage: `dotnet run input1.txt input2.txt`
+            var counts = (from fileName in args
+                          from line in File.ReadLines(fileName)
+                          group line by line into lines
+                          where lines.Count() > 1
+                          select lines)
+                         .ToDictionary(lines => lines.Key, lines => lines.Count());
+            
+            foreach (var lineCount in counts)
+            {
+                Console.WriteLine($"{lineCount.Value}\t{lineCount.Key}");
+            }
+        }
+    }
+}
