87f731358c5f3d95eeb4c42b35390ca8b171d10a
Victor Grigoriu <vgrigoriu@gmail.com>
2016-07-30 21:56:44 +0300

    Dup2: read from stdin or list of files

---
+++ 03.Dup2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Dup2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var counts = new Dictionary<string, int>();
+            if (args.Length == 0)
+            {
+                CountLines(Console.In, counts);
+            }
+            else
+            {
+                foreach (var fileName in args)
+                {
+                    try
+                    {
+                        using (var reader = File.OpenText(fileName))
+                        {
+                            CountLines(reader, counts);
+                        }
+                    }
+                    catch (IOException ex)
+                    {
+                        Console.Error.WriteLine($"Error opening {fileName}: {ex}");
+                    }
+                }
+            }
+
+            foreach (var line in counts.Keys)
+            {
+                if (counts[line] > 1)
+                {
+                    Console.WriteLine($"{counts[line]}\t{line}");
+                }
+            }
+        }
+
+        private static void CountLines(TextReader reader, Dictionary<string, int> counts)
+        {
+            string line;
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (!counts.ContainsKey(line))
+                {
+                    counts[line] = 0;
+                }
+
+                counts[line]++;
+            }
+        }
+    }
+}
