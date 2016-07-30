5119525f3db1cc07e7ff18cdc9b8138ff14a4c0c
Victor Grigoriu <vgrigoriu@gmail.com>
2016-07-30 18:43:51 +0300

    Dup1: read from stdin into a dictionary

---
+++ 03.Dup1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dup1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var counts = new Dictionary<string, int>();
+            string line;
+            while ((line = Console.In.ReadLine()) != null)
+            {
+                if (!counts.ContainsKey(line))
+                {
+                    counts[line] = 0;
+                }
+
+                counts[line]++;
+            }
+
+            foreach (var aLine in counts.Keys)
+            {
+                if (counts[aLine] > 1)
+                {
+                    Console.WriteLine($"{counts[aLine]}\t{aLine}");
+                }
+            }
+        }
+    }
+}
